ddf87fcfb2b29879a2c2f153bcb7fd32f5a39f5b
Z2VvZ2Vv <geoffrey03160@outlook.fr>
2024-10-18 11:05:18 +0200

    adding essentials functions

--- main.h
+++ main.h
@@ -1,6 +1,4 @@
 #include "stdio.h"
 
-<<<<<<< HEAD
-=======
 int chargerData(int tabNumEtu[], int tabRefStage[], int tabNoteFinale[], int tailleTab);
->>>>>>> e87738e (s)
+int rechercher(int val, int tab[], int nb, int *trouve);
