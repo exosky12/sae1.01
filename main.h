45c298158521b354257dffeac734fbdbc49be3b9
exosky12 <exoskyv2@gmail.com>
2024-11-07 08:31:03 +0100

    sdsd

--- main.h
+++ main.h
@@ -1,4 +1,15 @@
 #include "stdio.h"
 
-int chargerData(int tabNumEtu[], int tabRefStage[], int tabNoteFinale[], int tailleTab);
+int chargerData(int tabNumEtu[], int tabRefStage[], int tabNoteFinale[], int tabDept[], int tailleTab);
 int rechercher(int val, int tab[], int nb, int *trouve);
+void decalerADroite(int tab[], int indice, int nbMatieres);
+void decalerAGauche(int tab[], int indice, int nbMatieres);
+void sauvegarde(int tabMatieres[], int tabCoef[], int nbMatieres);
+int affichageGlobal(void);
+int affichageJury(void);
+void globalJury(void);
+void globalEtudiant(void);
+void globalResponsable(void);
+void clearScreen(void);
+int affichageEtudiant(int stageRestant);
+void global(void);
--- testmain.c
+++ testmain.c
@@ -1,8 +1,6 @@
 #include "main.h"
 
 int main(void){
-    int tabNumEtu[100], tabRefStage[100], tabNoteFinale[100], i;
-    i = chargerData(tabNumEtu, tabRefStage,tabNoteFinale, 100);
-    printf("%d", i);
+    global();
     return 0;
 }
